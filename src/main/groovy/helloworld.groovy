0fc4a2ae459491965497a5e90654d8833f6ab7e8
crr <crr@us.ibm.com>
2015-01-13 14:00:08 -0500

    Added a second version to show properties, upgrades, and how to use AirPluginTool to read input properties
    Change-Id: Ibd76fad74db3a95570441f06ec06416d3920f00d

--- src/main/groovy/helloworld.groovy
+++ src/main/groovy/helloworld.groovy
@@ -1,5 +1,13 @@
 import com.urbancode.air.*
 
+final airTool = new AirPluginTool(args[0], args[1])
+final def props = airTool.getStepProperties()
+
+final def name = props['name'] ?: "World"
+
 CommandHelper cmdHelper = new CommandHelper(new File("."))
-def command = ['echo', 'Hello  World']
+def command = ['echo']
+def message = "Hello ${name}!"
+command << message
+
 cmdHelper.runCommand("Running echo", command)
