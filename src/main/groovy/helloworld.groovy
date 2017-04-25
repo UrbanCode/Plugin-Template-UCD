ced929a0cf369ebc11f27c3e6d76b68c18a38a5b
Nicholas Mathison <nhmathis@us.ibm.com>
2017-04-25 16:43:41 -0400

    Test Changes

--- src/main/groovy/helloworld.groovy
+++ src/main/groovy/helloworld.groovy
@@ -1,6 +1,6 @@
 /* This is an example step groovy to show the proper use of APTool
- * In order to use import these utilities, you have to use the "pluginutilscripts" jar
- * that comes bundled with this plugin example. 
+ * In order to use import these utilities, you have to use the 'groovy-plugin-utils-1.1.jar'
+ * that is pulled down from Maven Central. 
  */
 
 import com.urbancode.air.AirPluginTool;
@@ -22,16 +22,16 @@ final def props = airTool.getStepProperties()
 /* This is how you retrieve properties from the object. You provide the "name" attribute of the 
  * <property> element. In this example, if property "name" is null or empty, it will default
  * to the the String "World"
- * 
  */
 final def name = props['name'] ?: "World"
 
-//example commandHelper
-CommandHelper cmdHelper = new CommandHelper(new File("."))
+// Create a list of strings which make up a command for the command line
 def command = ['echo']
 def message = "Hello ${name}!"
-command << message
+command << message          // command == ["echo", "Hello World!"]
 
+//Example of CommandHelper which helps construct and run a specified command in the Agent's terminal.
+CommandHelper cmdHelper = new CommandHelper(new File("."))
 cmdHelper.runCommand("Running echo", command)
 
 //Set an output property
